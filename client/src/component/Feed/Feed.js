9044eefae67b4c3e2cb826e4a682e81a614d4555
sopranoelfuego <aubinjaja@gmail.com>
2021-03-04 12:00:31 +0300

    startin Feed component

--- client/src/component/Feed/Feed.js
+++ client/src/component/Feed/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import MessageSender from "./messageSender/MessageSender"
 import Stories from "./Story/Stories"
 
 const Feed = () => {
     return (
         <div className="feed">
             <Stories/>
+            <MessageSender/>
             
         </div>
     )
--- client/src/component/Feed/messageSender/MessageSender.js
+++ client/src/component/Feed/messageSender/MessageSender.js
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
-import Avatar from "@material-ui/core/Avatar"
+import {Avatar,Input,Button} from "@material-ui/core"
+import "./MessageSender.css"
 
 const MessageSender = () => {
     const[input,setInput]=useState("")
@@ -10,25 +11,37 @@ const MessageSender = () => {
        }
    }
 
-   const handleSubmit=()=>{
+   const handleSubmit=(e)=>{
        e.preventDefault()
    }
 
     return (
         <div className="MessageSender">
-            <div className="MessageSende__top">
+            {/* THIS IS FOR MESSAGE INPUT FILE CHOOSE AND SUBMIT */}
+            <div className="MessageSender__top">
                 <Avatar src="https://scontent.fbjm1-1.fna.fbcdn.net/v/t1.0-9/44339331_2516636145229442_7134310788872798208_n.jpg?_nc_cat=108&ccb=3&_nc_sid=8bfeb9&_nc_ohc=t_50ioFoIukAX9YrOhk&_nc_ht=scontent.fbjm1-1.fna&oh=a32baa737f3663c6f2c4ff893a7c485d&oe=60649995"/>
-                <form>
+                <form onSubmit={handleSubmit} className="MessageSender__top__form">
 
-                    <label for="MessageSender__input">send</label>
+                    <div className="input__container">
                     <input type="text" placeholder="caporal chefu what is in your mind..." 
                       onChange={e => setInput(e.target.value)}
 
                     />
-                    <label for="MessageSender__file">choose file</label>
-                    <input type="file" onChange={getBackFile}/>
+                    </div>
+                    <label for="MessageSender__file" style={{paddingTop:"10px",color:"link"}}>
+                       choose file
+                    </label>
+                    <Input type="file" onChange={getBackFile} id="MessageSender__file" style={{display:"none"}}/>
+                    <Button type="submit">send</Button>
                 </form>
             </div>
+            {/* END OF MESSAGE */}
+            {/* MEDIA BUTTONS */}
+            <div className="mediaButton">
+
+
+            </div>
+
             
         </div>
     )
