176f0a3947d3cf8c24de494c25caf2dde4af1fac
sopranoelfuego <aubinjaja@gmail.com>
2021-03-05 07:17:42 +0300

    Post component disigning

--- client/src/component/Feed/Feed.js
+++ client/src/component/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MessageSender from "./messageSender/MessageSender"
 import Stories from "./Story/Stories"
+import Post from "./Post/Post"
 import "./feed.css"
 
 const Feed = () => {
@@ -8,6 +9,13 @@ const Feed = () => {
         <div className="feed">
             <Stories/>
             <MessageSender/>
+            <Post 
+              profilePic="https://scontent.fbjm1-1.fna.fbcdn.net/v/t1.0-9/147382502_3252376431655406_6044184046602460618_o.jpg?_nc_cat=109&ccb=3&_nc_sid=09cbfe&_nc_ohc=GtnjuoO9BNwAX8R4kRx&_nc_ht=scontent.fbjm1-1.fna&oh=6c5bf14fb4a3323f4a9a7ea8900b5e62&oe=6065DD2C"
+              imgName=""
+              timestamp=""
+              username="caporal"
+              message="mbega ga bsh..."
+            />
             
         </div>
     )
---
+++ client/src/component/Feed/Post/Post.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import {Avatar } from "@material-ui/core"
+import moment from "moment"
+import "./Post.css"
+import {ThumbUp,ChatBubbleOutlined,NearMe,ExpandMoreOutlined,AccountCircle, AddCommentRounded} from "@material-ui/icons"
+
+const Post = ({profilePic,imgName,timestamp,message,username}) => {
+    return (
+        <div className="post">
+            {/* POST TOP */}
+            <div className="post__top">
+                <Avatar src={profilePic} className="post__avatar" />
+                <div className="post__info">
+                    <h4>{username}</h4>
+                     <p>{moment().format("MM Do YY")}</p>
+                </div>
+
+
+               
+            </div>
+            {/* END OF POST TOP */}
+            {/* POST MESSAGE */}
+            <div className="post__message">
+               <p> {message}</p>
+            </div>
+            {/* POST OPTIONS */}
+            <div className="post__options">
+                    <div className="post__option">
+                        <ThumbUp/>
+                        <p>like</p>
+                    </div>
+                    <div className="post__option">
+                            <ChatBubbleOutlined/>
+                            <p>comments</p>
+                    </div>
+                    <div className="post__option">
+                                <NearMe/>
+                                <p>share</p>
+                    </div>
+                    
+                    <div className="post__option">
+                                <AddCommentRounded/>
+                                <ExpandMoreOutlined/>
+                    </div>
+                    
+                </div>
+                {/* END OF POST OPTIONS */}
+            
+        </div>
+    )
+}
+
+export default Post
